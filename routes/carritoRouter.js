60b9fcaacf2b1ad79534302be819fd07e3ca4285
PADILLA <paca23919@gmail.com>
2025-03-20 07:47:01 -0600

    casi listo

--- routes/carritoRouter.js
+++ routes/carritoRouter.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { agregarAlCarrito, obtenerCarritoPorUsuario } = require('../controllers/carrito');
+const {
+    agregarAlCarrito,
+    obtenerCarritoPorUsuario,
+    eliminarDelCarrito,
+    actualizarCarrito,
+} = require('../controllers/carrito');
 const router = express.Router();
 
 // Ruta para agregar un producto al carrito
@@ -7,5 +12,11 @@ router.post('/carrito', agregarAlCarrito);
 
 // Ruta para obtener el carrito de un usuario por su ID
 router.get('/carrito/:id_usuario', obtenerCarritoPorUsuario);
+
+// Ruta para eliminar un producto del carrito
+router.delete('/carrito/:id_carrito', eliminarDelCarrito);
+
+// Ruta para actualizar la cantidad de un producto en el carrito
+router.put('/carrito/:id_carrito', actualizarCarrito);
 
 module.exports = router;
